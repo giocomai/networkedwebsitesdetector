e3ee871b6d4aa4c3dce72e908729bda1e9c75f63
Giorgio Comai <g@giorgiocomai.eu>
2019-04-06 12:06:27 +0200

    introduce check_if_exists() #newfunction

--- R/get_website_data.R
+++ R/get_website_data.R
@@ -12,15 +12,31 @@ get_screenshot <- function(url, width = 1280, height = 1280, language = NULL) {
   dir.create(path = file.path("data", "domains"), showWarnings = FALSE)
   dir.create(path = file.path("data", "domains", "screenshots"), showWarnings = FALSE)
   dir.create(path = file.path("data", "domains", "screenshots", language), showWarnings = FALSE)
+  dir.create(path = file.path("data", "domains", "screenshots_failed"), showWarnings = FALSE)
+  dir.create(path = file.path("data", "domains", "screenshots_failed", language), showWarnings = FALSE)
+  
   today_path <- file.path("data", "domains", "screenshots", language, Sys.Date())
+  today_path_screenshots_failed <- file.path("data", "domains", "screenshots_failed", language, Sys.Date())
   dir.create(path = today_path, showWarnings = FALSE)
+  dir.create(path = today_path_screenshots_failed, showWarnings = FALSE)
   
   pb <- dplyr::progress_estimated(length(url))
-  purrr::walk(.x = paste0("http://", url),
+  purrr::walk(.x = url,
               .f =  function(x) {pb$tick()$print()
-                webshot::webshot(url = x,
-                                 file = file.path(today_path, paste0(stringr::str_remove(string = x, pattern = stringr::fixed("http://")), ".png")),
-                                 vwidth = width,
-                                 vheight = height,
-                                 cliprect = "viewport")})
+                check <- check_if_exists(domain = x, type = "screenshots")
+                if (sum(check$failed))
+                tryCatch({webshot::webshot(url = paste0("http://", x),
+                                           file = file.path(today_path, paste0(x, ".png")),
+                                           vwidth = width,
+                                           vheight = height,
+                                           cliprect = "viewport")},
+                         error=function(e){
+                           
+                           readr::write_file(x = paste0("Could not download ", x),
+                                             path = file.path(today_path_screenshots_failed, paste0(stringr::str_remove(string = x, pattern = stringr::fixed("http://")), ".txt")))
+                           
+                           message(paste0("Could not download ", x, ": "),
+                                   conditionMessage(e), "\n")})
+              }
+  )
 }
--- R/utils.R
+++ R/utils.R
@@ -7,3 +7,56 @@
 #' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
 NULL
+
+
+#' Check if corresponding information for a given domain exists for a given data type and a given time period
+#'
+#' @param domain A domain name (e.g. "example.com").
+#' @param type Type of file to check, defaults to "homepage". Alternative options: "screenshots".
+#' @return Nothing, used for its side effects. 
+#' @examples
+#' 
+#' @export
+#' 
+
+check_if_exists <- function(domain,
+                            type = "homepage",
+                            language = NULL,
+                            since = Sys.Date()-31,
+                            simplify = TRUE) {
+    
+  if (is.null(language)==TRUE) {
+    language <- list.dirs(file.path("data", "domains", "homepage"), recursive = FALSE, full.names = FALSE)
+    if (length(language)!=1) {
+      stop("More than one language found. Please select one language.")
+    }
+  }
+  
+  base_path <- file.path("data", "domains", type, language)
+  base_path_failed <- file.path("data", "domains", paste0(type, "_failed"), language)
+  
+  available_files <- fs::dir_info(path = c(base_path, base_path_failed),
+                                  recursive = FALSE,
+                                  type = "directory") %>% 
+    dplyr::transmute(date = as.Date(stringr::str_remove(string = stringr::str_remove(path, pattern = paste0(base_path_failed, "/")), pattern = paste0(base_path, "/")))) %>% 
+    dplyr::distinct(date) %>% 
+    dplyr::filter(date>as.Date(since)) %>% 
+    dplyr::mutate(potential_file_location = file.path(base_path,
+                                                      date,
+                                                      paste0(domain, ".html")), 
+                  potential_failed_file_location = file.path(base_path_failed,
+                                                             date,
+                                                             paste0(domain, ".txt"))) %>% 
+    dplyr::mutate(available = fs::file_exists(path = potential_file_location), 
+                  failed = fs::file_exists(path = potential_failed_file_location)) 
+  
+  if (simplify==FALSE) {
+    return(available_files)
+  } else if (simplify==TRUE) {
+    if (sum(available_files$available, available_files$available)>0) {
+      return(TRUE)
+    } else (
+      return(FALSE)
+    )
+  }
+}
