a890d2ba013c56e767fade992249af7fba61998b
Giorgio Comai <g@giorgiocomai.eu>
2019-05-11 14:04:11 +0200

    new default to available languages

--- R/extract_keywords.R
+++ R/extract_keywords.R
@@ -8,41 +8,61 @@
 #' 
 #' @export
 
-nwd_extract_keywords <- function(languages = c("ar","bg","cs","da","de","el","en","es", "et","fi","fr","hr", "hu","it", "lt","lv","nl","pl","pt","ro","ru", "sk","sl","sv","sw","tr","zh"),
-                             n = 10,
-                             date = Sys.Date(), 
-                             store = TRUE) {
+nwd_extract_keywords <- function(languages = NULL,
+                                 n = 20,
+                                 date = NULL, 
+                                 store = TRUE) {
+  
+  if (is.null(languages)) {
+    languages <-  fs::dir_ls(path = fs::path("emm_newsbrief"),
+                             recurse = FALSE,
+                             type = "directory") %>% 
+      fs::path_file()
+  }
+  
   
   for (i in languages) {
-    all_rds <- list.files(path = fs::path("emm_newsbrief",
-                                           i,
-                                           as.character(date)),
-                          pattern = paste0(i, "\\.rds"),
-                          full.names = TRUE)  
-
-    all_news <- suppressMessages(purrr::map_df(.x = all_rds,
-                                               .f = readr::read_rds)) %>% #unisce i file in un singolo data frame
-      dplyr::distinct(link, .keep_all = TRUE)
     
-    keywords <- all_news %>% 
-      dplyr::transmute(title, Date = as.Date(pubDate)) %>%
-      tidytext::unnest_tokens(input = title, output = "words") %>% 
-      dplyr::anti_join(tibble::tibble(words = stopwords::stopwords(language = i, source = "stopwords-iso")), by = "words") %>% # elimina stopwords
-      dplyr::filter(!stringr::str_detect(string = words, pattern = "[[:digit:]]")) %>%  # togliere i numeri registrati come parole
-      dplyr::group_by(words) %>% 
-      dplyr::count(sort = TRUE) %>% 
-      head(n) 
-    
-    if (store==TRUE) {
-      keywords_base_path <- fs::path("keywords", 
-                                     i,
-                                     as.character(date))
+    if (is.null(date)) {
+      date <- fs::dir_ls(path = fs::path("emm_newsbrief", i), recurse = FALSE, type = "directory") %>% 
+        fs::path_file()
       
-      fs::dir_create(path = keywords_base_path)
-      saveRDS(object = keywords,
-              file = fs::path(keywords_base_path,
-                              paste0(date, "-keywords_", i, ".rds")))
     }
+    
+    for (j in date) {
+      all_rds <- list.files(path = fs::path("emm_newsbrief",
+                                            i,
+                                            as.character(j)),
+                            pattern = paste0(i, "\\.rds"),
+                            full.names = TRUE)  
+      
+      all_news <- suppressMessages(purrr::map_df(.x = all_rds,
+                                                 .f = readr::read_rds)) %>% #unisce i file in un singolo data frame
+        dplyr::distinct(link, .keep_all = TRUE)
+      
+      keywords <- all_news %>% 
+        dplyr::transmute(title, Date = as.Date(pubDate)) %>%
+        tidytext::unnest_tokens(input = title, output = "words") %>% 
+        dplyr::anti_join(tibble::tibble(words = stopwords::stopwords(language = i, source = "stopwords-iso")), by = "words") %>% # elimina stopwords
+        dplyr::filter(!stringr::str_detect(string = words, pattern = "[[:digit:]]")) %>%  # togliere i numeri registrati come parole
+        dplyr::group_by(words) %>% 
+        dplyr::count(sort = TRUE) %>% 
+        head(n) 
+      
+      if (store==TRUE) {
+        keywords_base_path <- fs::path("keywords", 
+                                       i,
+                                       as.character(j))
+        
+        fs::dir_create(path = keywords_base_path)
+        saveRDS(object = keywords,
+                file = fs::path(keywords_base_path,
+                                paste0(j, "-keywords_", n, "_", i, ".rds")))
+      }
+      
+    }
+    
+    
   }
   
   keywords
